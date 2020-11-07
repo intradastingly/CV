8eedc35fa38a7df67b4592ab8f847f153bc0e5e5
Asaf Läckgren <asaf_lackgren@hotmail.com>
2020-11-07 11:29:32 +0100

    added js boilercode

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 windows.onload('load', main);
 
 function main(){
-
+    eventListeners();
 }
 
+function eventListeners() {
+    let size = document.querySelector('nav');
+    size.classList.toggle("menuOut");
+}
+
+function toggleSection(){
+    //select different tab for expansion.
+    //assign css class to selected element.
+    //use for each to iterate over.
+    //assign event for when clicked.
+}

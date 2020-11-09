621e400a8c7801c1a7e390c19edeb308f5e7dcc9
Asaf Läckgren <asaf_lackgren@hotmail.com>
2020-11-09 12:45:00 +0100

    Js changes

--- script.js
+++ script.js
@@ -8,16 +8,23 @@ function toggleTabs() {
     const dropDown = document.getElementsByClassName("drop-down-tabs");
     for(const tabs of dropDown){
         tabs.onclick = toggleSection;
+        return tabs;
     }
 }
 
-function toggleSection(){
+function toggleSection(tabs){
     const sections = document.getElementsByClassName('section');
     for(let i = 0; i < sections.length; i++){
         if (sections[i].classList.contains('display')) {
             sections[i].classList.remove('display');
+            console.log('tabs')
         } else {
             sections[i].classList.add('display')
         }
     }
 }   
+
+const aboutMe = document.getElementById('aboutMe');
+const skills = document.getElementById('skills');
+const portfolio = document.getElementById('portfolio');
+const socialMedia = document.getElementById('socialMedia');

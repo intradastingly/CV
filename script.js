6c16c0c8d01d18a7d8afc7132217126c130374be
Asaf Läckgren <asaf_lackgren@hotmail.com>
2020-11-10 12:03:59 +0100

    can't figure it out

--- script.js
+++ script.js
@@ -12,56 +12,59 @@ function toggleTabs() {
     aboutMe.addEventListener('click', aboutMeSection);
     skills.addEventListener('click', skillsSection);
     portfolio.addEventListener('click', portfolioSection);
-    socialMedia.addEventListener('click', socialMediaSection);
-    
+    socialMedia.addEventListener('click', socialMediaSection); 
 }
 
 function aboutMeSection(){
     const about = document.getElementById('aboutMe-expand');
     about.classList.toggle('display');
-    removePreviousSelection()
+    removePreviousSelection;
 }
 
 function skillsSection(){
     const skills = document.getElementById('skills-expand');
     skills.classList.toggle('display');
-    removePreviousSelection()
+    removePreviousSelection;
 }
 
 function portfolioSection(){
     const portofolio = document.getElementById('portfolio-expand');
     portofolio.classList.toggle('display');  
-    removePreviousSelection()
+    removePreviousSelection;
 }
 
 function socialMediaSection(){
     const socialMedia = document.getElementById('socialMedia-expand');
-    socialMedia.classList.toggle('display');
-    removePreviousSelection()
+    socialMedia.classList.toggle('display');  
+    removePreviousSelection;
 }
 
 function removePreviousSelection(){
     const sections = document.getElementsByClassName('section');
-    for(let i = 0; i < sections.length; i++){
-        if(sections[i].classList.contains('display')){  
-        sections[i].classList.add('display');
-    }
-}
-}
-/* function toggleSection(){
-    const sections = document.getElementsByClassName('section');
-    for(let i = 0; i < sections.length; i++){    
-        if (sections[i].classList.contains('display')) {
-            sections[i].classList.remove('display');
-        } else {
+    for(let i = 0; i < sections.length; i++){   
+        if (!sections[i].classList.contains('display')){
             sections[i].classList.add('display')
         }
     }
-   
-}   */ 
+}
+
 
 /* function toggleTabs() {
     const dropDown = document.getElementsByClassName("drop-down-tabs");
     for(let i = 0; i < dropDown.length; i++){
-        dropDown[i].onclick = toggleSection; 
-}  */
+        dropDown[i].onclick = toggleSection;
+    }   
+}
+
+function toggleSection(dropDown){
+    const sections = document.getElementsByClassName('section');
+    for(let i = 0; i < sections.length; i++){    
+        if (!sections[i].classList.contains('display')){
+            sections[i].classList.add('display')
+        } sections[i].classList.remove('display');
+            } else {
+        sections[i].classList.add('display')
+        }
+    }   
+}
+ */
